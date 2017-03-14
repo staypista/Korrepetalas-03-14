4f3709e7bba99193878ad5a5ee0a2bf9dde2cddf
Nagy Zsolt <stay.pista@gmail.com>
2017-03-14 22:31:40 +0100

    Feladatkiírás

---
+++ Korrepetalas-03-14/main.c
@@ -0,0 +1,69 @@
+/**
+ * main.c
+ *
+ * Created: 2017. 03. 14. 22:04:32
+ * Author : Nagy Zsolt
+ */
+ #include <stdio.h>
+#include <stdlib.h>
+
+/**Programozás korrepetálás első heti házifeladat.
+ * Téma: syntaxis és alap algoritmus gyakorlás.
+ * Kérem a teljesítési időket rögzítse!
+ * A feladatokat a kiírás alá végezze el.
+ * Célidő: 11 perc + 4 perc(bónusz)
+ * Superuser idő: 5 perc
+ */
+
+int main()
+{
+    /** 1. Feladat:
+     *  Adja össze "a" és "b" változót úgy hogy az eredmény "c" változóban képződjön.
+     *  Szorozza össze "a" és "b" változót úgy hogy az eredmény "c" változóban képződjön.
+     *  Az eredményeket printf függvénnyel írassa ki mindkét esetben.
+     *  Célidő: 2 perc
+     *  Teljesítési idő:
+     */
+    unsigned char a=10, b=39, c=0;
+
+
+    /** 2. Feladat:
+     *  Ossza el "a" és "b" változót úgy hogy az osztás eredményét és a maradékot is
+     *  kiiratja printf függvénnyel.
+     *  Célidő: 3 perc
+     *  Teljesítési idő:
+     */
+    a=193;
+    b=7;
+
+
+    /** 3. Feladat:
+     *  Készítsen egy olyan feltételt amely megvizsgálja "a" változó nagyobb-e "b"
+     *  változónál. Ha az állítás igaz printf függvénnyel írjon ki egy tetszőleges
+     *  szöveget a képernyőre.
+     *  Célidő: 3 perc
+     *  Teljesítési idő:
+     */
+
+
+     /** 4. Feladat:
+     *  Készítsen egy olyan ciklust mely számokat ír ki printf függvénnyel 10-től
+     *  57-ig. A számokat tabulárral válassza el egymástól.
+     *  tabulátor -> "\t"  sorelemelés -> "\n"
+     *  Célidő: 3 perc
+     *  Teljesítési idő:
+     */
+
+
+     /** Bónusz Feladat:
+     *  Készítsen egy olyan függvényt mely a bemenetében vár 2 változót és a
+     *  visszatérési értékben a 2 beadott változó replusszával tér vissza.
+     *
+     *  Célidő: infinit perc
+     *  Teljesítési idő:
+     */
+
+
+
+    return 0;
+}
