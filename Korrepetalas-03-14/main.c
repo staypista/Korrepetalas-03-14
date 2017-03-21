647477352a853032c32f98ecda29e2a9198432a0
Nagy Zsolt <stay.pista@gmail.com>
2017-03-21 21:03:44 +0100

    Megoldás

--- Korrepetalas-03-14/main.c
+++ Korrepetalas-03-14/main.c
@@ -4,7 +4,7 @@
  * Created: 2017. 03. 14. 22:04:32
  * Author : Nagy Zsolt
  */
- #include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 /**Programozás korrepetálás első heti házifeladat.
@@ -15,8 +15,11 @@
  * Superuser idő: 5 perc
  */
 
+ double replusz(double r1, double r2);
+
 int main()
 {
+    printf("*** 1. feladat ***\n");
     /** 1. Feladat:
      *  Adja össze "a" és "b" változót úgy hogy az eredmény "c" változóban képződjön.
      *  Szorozza össze "a" és "b" változót úgy hogy az eredmény "c" változóban képződjön.
@@ -25,8 +28,11 @@ int main()
      *  Teljesítési idő:
      */
     unsigned char a=10, b=39, c=0;
+    int eredmeny1 = a + b;
+    int eredmeny2 = a * b;
+    printf("Osszeg:%d \t Szorzat:%d", eredmeny1, eredmeny2);
 
-
+    printf("\n\n*** 2. feladat ***\n");
     /** 2. Feladat:
      *  Ossza el "a" és "b" változót úgy hogy az osztás eredményét és a maradékot is
      *  kiiratja printf függvénnyel.
@@ -35,8 +41,10 @@ int main()
      */
     a=193;
     b=7;
+    double eredmeny3 = a/b;
+    printf("Hanyados:%3.5f", eredmeny3);
 
-
+    printf("\n\n*** 3. feladat ***\n");
     /** 3. Feladat:
      *  Készítsen egy olyan feltételt amely megvizsgálja "a" változó nagyobb-e "b"
      *  változónál. Ha az állítás igaz printf függvénnyel írjon ki egy tetszőleges
@@ -45,7 +53,11 @@ int main()
      *  Teljesítési idő:
      */
 
+    if(a > b){
+        printf("Tetszoleges szoveg");
+    }
 
+    printf("\n\n*** 4. feladat ***\n");
      /** 4. Feladat:
      *  Készítsen egy olyan ciklust mely számokat ír ki printf függvénnyel 10-től
      *  57-ig. A számokat tabulárral válassza el egymástól.
@@ -53,8 +65,10 @@ int main()
      *  Célidő: 3 perc
      *  Teljesítési idő:
      */
+    int i;
+    for(i=10;i<57;i++) printf("%d\t", i);
 
-
+     printf("\n\n*** Bunusz feladat ***\n");
      /** Bónusz Feladat:
      *  Készítsen egy olyan függvényt mely a bemenetében vár 2 változót és a
      *  visszatérési értékben a 2 beadott változó replusszával tér vissza.
@@ -62,8 +76,18 @@ int main()
      *  Célidő: infinit perc
      *  Teljesítési idő:
      */
+    double eredmeny4 = replusz(3.14, 12.64);
 
+    printf("%f", eredmeny4);
 
 
     return 0;
+}
+
+double replusz(double r1, double r2){
+    double szamlalo, nevezo, eredmeny;
+    szamlalo = r1 * r2;
+    nevezo = r1 + r2;
+    eredmeny = szamlalo / nevezo;
+    return eredmeny;
 }
